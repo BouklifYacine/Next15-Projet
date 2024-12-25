cf3311f7b0765cc2461acf742c927984f03481f5
YacineBoukliff <yacine69800@live.fr>
2024-12-25 11:49:17 +0100

    Creation de l'api post des taches

--- next15/app/api/crud/route.ts
+++ next15/app/api/crud/route.ts
@@ -1,12 +1,19 @@
 import SchemaTaches from "@/app/schema/schema-tache";
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/db";
 
 export async function POST(request: NextRequest){
     const {titre, message } = await request.json()
    const validation =  SchemaTaches.safeParse({titre, message})
 
    if (!validation.success) {
-    NextResponse.json({ Message : " Vous devez respecter les règles khey "})
+    NextResponse.json(validation.error.format(), {status : 400})
    }
+   const nouvelletache =  await prisma.tache.create({
+    data : {titre, message}
+   })
+
+   return NextResponse.json(nouvelletache)
+
 
 }

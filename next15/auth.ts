9447be7f218b3b17244b5fbcac1b5876e891889a
YacineBoukliff <yacine69800@live.fr>
2024-12-25 19:41:14 +0100

    Ajout de la logique des credentials et ajout de password dans user pour les credentials

--- next15/auth.ts
+++ next15/auth.ts
@@ -1,13 +1,50 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 import { prisma } from "@/prisma";
+import bcrypt from "bcryptjs";
 
- 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-  providers: [GitHubProvider({
-    clientId: process.env.AUTH_GITHUB_ID,
-    clientSecret: process.env.AUTH_GITHUB_SECRET
-  })],
-})
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Credentials({
+      name: "Formulaire",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials, request) {
+        const email = credentials.email as string | undefined;
+        const password = credentials.password as string | undefined;
+
+        if (!email || !password) {
+          throw new Error(" Vous devez remplir les deux champs ");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (!user) {
+          throw new Error(" Utilisateur introuvable ");
+        }
+
+        const MotDePasse = await bcrypt.compare(password, user.password);
+
+        if (!MotDePasse) {
+          throw new Error("Les mots de passes correspondent pas");
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+        };
+      },
+    }),
+  ],
+});

03cc1826bd6d0844df850fb757fb44e13a3b83f0
YacineBoukliff <yacine69800@live.fr>
2024-12-25 12:50:04 +0100

    Gestion dynamique des erreurs et meilleur validation sur le schéma initial

--- next15/app/crud/page.tsx
+++ next15/app/crud/page.tsx
@@ -17,7 +17,7 @@ interface Formulaire {
 
 const Crud = () => {
 
-  const {register, handleSubmit, reset, formState: {isSubmitting} } = useForm<Formulaire>({
+  const {register, handleSubmit, reset, formState: {isSubmitting, errors} } = useForm<Formulaire>({
     resolver: zodResolver(SchemaTaches),
   })
 
@@ -35,16 +35,19 @@ const Crud = () => {
     <h1 className='text-center text-3xl font-bold text-blue-500 mb-4'> 
       Créer votre tâche 
     </h1>
+    <p> </p>
     
     <form onSubmit={handleSubmit(OnSubmit)} className='space-y-4'>
       <div className='space-y-2'>
         <Label htmlFor="titre">Titre</Label>
         <Input type="text" id="titre" placeholder="Titre" className="w-full" {...register("titre")} />
+        {errors.titre && <p className='text-red-500'> {errors.titre?.message} </p>}
       </div>
 
       <div className='space-y-2'>
         <Label htmlFor="message">Message</Label>
         <Input type="text" id="message" placeholder="Message" className="w-full" {...register("message")} />
+        {errors.message && <p className='text-red-500'> {errors.message?.message} </p>}
       </div>
 
       <Button disabled={isSubmitting} type="submit" className='w-full bg-blue-500 hover:bg-blue-600'>
--- next15/app/schema/schema-tache.ts
+++ next15/app/schema/schema-tache.ts
@@ -1,8 +1,16 @@
 import {z} from "zod"
 
 const SchemaTaches = z.object({
-    titre: z.string().trim().min(3, {message : " Minimum 3 caractères "}).max(35, {message : " Maximum 35 caractères"}),
-    message : z.string().trim().min(3, {message : " Minimum 3 caractères "}).max(255, {message : " Maximum 255 caractères"}),
+    titre: z.string()
+        .trim()
+        .min(1, "Titre est requis")
+        .min(3, "Minimum 3 caractères")
+        .max(35, "Maximum 35 caractères"),
+    message: z.string()
+        .trim()
+        .min(1, "Message est requis")
+        .min(3, "Minimum 3 caractères")
+        .max(255, "Maximum 255 caractères"),
 })
 
 export default SchemaTaches

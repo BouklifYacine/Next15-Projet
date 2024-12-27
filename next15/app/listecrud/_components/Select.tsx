c242b14a6a2cfc514f8d31988d377aa296650a57
YacineBoukliff <yacine69800@live.fr>
2024-12-27 12:36:40 +0100

    Ajout de UseQuery pour fetch les users avec TanStack Query

--- next15/app/listecrud/_components/Select.tsx
+++ next15/app/listecrud/_components/Select.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import {
     Select,
@@ -8,19 +10,33 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { User } from '@prisma/client'
 
 const SelectComponent = () => {
+
+  const {data: utilisateurs, isLoading, error} = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get('/api/utilisateurs').then(res => res.data),
+    retry : 3,
+    staleTime: 60 * 1000
+  })
+
+  if(isLoading) return <p>Ca charge Kheyou </p>
+  
   return (
  <div>
       <Select>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Utilisateur" />
       </SelectTrigger>
-      <SelectContent>
+      <SelectContent> 
         <SelectGroup>
           <SelectLabel>Suggestions</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
+          {utilisateurs?.map(utilisateur => (
+               <SelectItem value={utilisateur.name || ""} key={utilisateur.id}>{utilisateur.name}</SelectItem>
+          ))}
         </SelectGroup>
       </SelectContent>
     </Select>
